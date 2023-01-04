9856a3112eeddbf609d7e2eb522fa7a041fb7a9f
CarlosCarrion98 <c.carrionmartinez1998@gmail.com>
2023-01-04 19:56:14 +0100

    Nuevas dependencias Maven y añadido un archivo config de hibernate

--- src/main/java/org/bd/dao/GenericDAO.java
+++ src/main/java/org/bd/dao/GenericDAO.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.List;
 
 public interface GenericDAO<T,ID extends Serializable> {
-     T create();
      void saveOrUpdate(T entity);
      T get(ID id);
      void delete(ID id);
