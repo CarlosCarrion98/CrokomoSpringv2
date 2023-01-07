9bfa382d82c842ed27dd4d3f1cce4d2a573a0698
CarlosCarrion98 <c.carrionmartinez1998@gmail.com>
2023-01-07 19:15:19 +0100

    Modificación de la sesión de ClienteDAOImpl

--- src/main/java/org/bd/dao/impl/ClienteDAOImpl.java
+++ src/main/java/org/bd/dao/impl/ClienteDAOImpl.java
@@ -2,12 +2,20 @@ package org.bd.dao.impl;
 
 import org.bd.dao.ClienteDAO;
 import org.bd.hibernate.config.HibernateConfig;
+import org.hibernate.cfg.Configuration;
 import org.objects.Cliente;
+import org.objects.Proyecto;
+import org.objects.relations.ClienteRequisito;
 
 public class ClienteDAOImpl extends GenericDAOImpl<Cliente, Integer> implements ClienteDAO {
 	
 	public ClienteDAOImpl() {
 		hc = new HibernateConfig<Cliente>(new Cliente());
+		hc.setMySession(new Configuration().configure()
+				.addAnnotatedClass(Cliente.class)
+				.addAnnotatedClass(ClienteRequisito.class)
+				.addAnnotatedClass(Proyecto.class)
+				.buildSessionFactory());
 	}
 	
 }
