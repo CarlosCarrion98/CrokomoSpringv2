7d7ceff2060d3ba4785a03ee2c6b5dbb2950bbac
CarlosCarrion98 <c.carrionmartinez1998@gmail.com>
2023-01-08 16:44:43 +0100

    Solucionado el problema que hacía no aparecer el formato web

--- src/main/java/org/CrokomoSpringv2/config/App.java
+++ src/main/java/org/CrokomoSpringv2/config/App.java
@@ -1,6 +1,7 @@
 package org.CrokomoSpringv2.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,7 +9,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-
+@ComponentScan(basePackages = "org.CrokomoSpringv2")
 public class App {
 	@Bean
 	public ViewResolver viewResolver() {
